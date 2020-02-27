a1ed24b13ffa33428877e650da896536d6c7eaae
Igor Raskin <packuh@gmail.com>
2020-02-27 13:32:12 +0500

    Create kvm-name_POST.sh

---
+++ kvm-name_POST.sh
@@ -0,0 +1,5 @@
+  
+##   Copyright [2019-06-03] [Raskin Igor Vladimirovich (RIV)]
+##   Licensed under the Apache License, Version 2.0 (the "License");
+##  For DataProtector this sript must be here /opt/omni/lbin
+/var/lib/libvirt/images/storage_kvm_os/backup_online_post. kvm-name
